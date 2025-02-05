bc98648c22fda78ce18c0e2327bb64d81b088ec4
MeiNanziiii <meinanziiii.ua@gmail.com>
2025-02-05 16:38:20 +0200

    keyframe better rendering

--- src/main/kotlin/ua/mei/cinefabric/gui/CutsceneGui.kt
+++ src/main/kotlin/ua/mei/cinefabric/gui/CutsceneGui.kt
@@ -3,6 +3,8 @@ package ua.mei.cinefabric.gui
 import eu.pb4.sgui.api.elements.GuiElementBuilder
 import eu.pb4.sgui.api.gui.HotbarGui
 import it.unimi.dsi.fastutil.ints.IntArrayList
+import net.minecraft.block.BlockState
+import net.minecraft.block.Blocks
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.data.DataTracker
 import net.minecraft.entity.decoration.DisplayEntity
@@ -14,15 +16,17 @@ import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket
 import net.minecraft.network.packet.s2c.play.EntityTrackerUpdateS2CPacket
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.math.Vec3d
+import org.joml.Vector3d
+import org.joml.Vector3f
 import ua.mei.cinefabric.scene.Keyframe
+import ua.mei.cinefabric.util.copy
 import java.util.*
+import kotlin.math.atan2
+import kotlin.math.sqrt
 
 class CutsceneGui(player: ServerPlayerEntity) : HotbarGui(player) {
     private val keyframes: MutableList<Keyframe> = mutableListOf()
     private val activeIds: MutableList<Int> = mutableListOf()
-
-    private val elements: MutableList<Int> = mutableListOf()
-    private var nextId: Int = -1
 
     init {
         setSlot(
@@ -37,45 +41,125 @@ class CutsceneGui(player: ServerPlayerEntity) : HotbarGui(player) {
             4,
             GuiElementBuilder.from(Items.EMERALD.defaultStack)
                 .setCallback { ->
-                    val id: Int = --nextId
-
-                    player.networkHandler.sendPacket(
-                        BundleS2CPacket(
-                            listOf(
-                                EntitySpawnS2CPacket(
-                                    id,
-                                    UUID.randomUUID(),
-                                    player.x,
-                                    player.y,
-                                    player.z,
-                                    0f,
-                                    0f,
-                                    EntityType.ITEM_DISPLAY,
-                                    0,
-                                    Vec3d.ZERO,
-                                    0.0
-                                ),
-                                EntityTrackerUpdateS2CPacket(
-                                    id,
-                                    listOf(
-                                        DataTracker.SerializedEntry<ItemStack>(
-                                            DisplayEntity.ItemDisplayEntity.ITEM.id,
-                                            DisplayEntity.ItemDisplayEntity.ITEM.dataType,
-                                            Items.DIAMOND.defaultStack
-                                        )
-                                    )
-                                )
-
-                            )
-                        )
+                    keyframes += Keyframe(
+                        player.x,
+                        player.y,
+                        player.z,
+                        player.yaw,
+                        player.pitch,
+                        500
                     )
 
-                    elements += id
+                    render()
                 }
         )
     }
 
+    fun render() {
+        player.networkHandler.sendPacket(EntitiesDestroyS2CPacket(IntArrayList.wrap(activeIds.toIntArray())))
+        activeIds.clear()
+
+        var entityId: Int = -1
+
+        keyframes.forEach {
+            val id: Int = --entityId
+
+            player.networkHandler.sendPacket(
+                BundleS2CPacket(
+                    listOf(
+                        EntitySpawnS2CPacket(
+                            id,
+                            UUID.randomUUID(),
+                            it.x,
+                            it.y,
+                            it.z,
+                            it.pitch,
+                            it.yaw,
+                            EntityType.ITEM_DISPLAY,
+                            0,
+                            Vec3d.ZERO,
+                            0.0
+                        ),
+                        EntityTrackerUpdateS2CPacket(
+                            id,
+                            listOf(
+                                DataTracker.SerializedEntry<ItemStack>(
+                                    DisplayEntity.ItemDisplayEntity.ITEM.id,
+                                    DisplayEntity.ItemDisplayEntity.ITEM.dataType,
+                                    Items.FIREWORK_STAR.defaultStack
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+
+            activeIds += id
+        }
+        if (keyframes.size > 1) {
+            (1..<keyframes.size).forEach {
+                val id: Int = --entityId
+
+                val previousKeyframe: Keyframe = keyframes[it - 1]
+                val currentKeyframe: Keyframe = keyframes[it]
+
+                val previousVector: Vector3d = Vector3d(previousKeyframe.x, previousKeyframe.y, previousKeyframe.z)
+                val currentVector: Vector3d = Vector3d(currentKeyframe.x, currentKeyframe.y, currentKeyframe.z)
+
+                val mid: Vector3d = previousVector.copy().add(currentVector).mul(0.5)
+                val diff: Vector3d = currentVector.copy().sub(previousVector)
+
+                val length: Float = diff.length().toFloat()
+
+                val yaw: Float = Math.toDegrees(atan2(-diff.x, diff.z)).toFloat() + 180f
+                val pitch: Float = Math.toDegrees(atan2(diff.y, sqrt(diff.x * diff.x + diff.z * diff.z))).toFloat()
+
+                player.networkHandler.sendPacket(
+                    BundleS2CPacket(
+                        listOf(
+                            EntitySpawnS2CPacket(
+                                id,
+                                UUID.randomUUID(),
+                                mid.x,
+                                mid.y,
+                                mid.z,
+                                pitch,
+                                yaw,
+                                EntityType.BLOCK_DISPLAY,
+                                0,
+                                Vec3d.ZERO,
+                                0.0
+                            ),
+                            EntityTrackerUpdateS2CPacket(
+                                id,
+                                listOf(
+                                    DataTracker.SerializedEntry<BlockState>(
+                                        DisplayEntity.BlockDisplayEntity.BLOCK_STATE.id,
+                                        DisplayEntity.BlockDisplayEntity.BLOCK_STATE.dataType,
+                                        Blocks.LIME_CONCRETE.defaultState
+                                    ),
+                                    DataTracker.SerializedEntry<Vector3f>(
+                                        DisplayEntity.SCALE.id,
+                                        DisplayEntity.SCALE.dataType,
+                                        Vector3f(0.05f, 0.05f, length)
+                                    ),
+                                    DataTracker.SerializedEntry<Vector3f>(
+                                        DisplayEntity.TRANSLATION.id,
+                                        DisplayEntity.TRANSLATION.dataType,
+                                        Vector3f(-0.025f, -0.025f, length / -2)
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+
+                activeIds += id
+            }
+        }
+    }
+
     override fun onClose() {
-        player.networkHandler.sendPacket(EntitiesDestroyS2CPacket(IntArrayList.wrap(elements.toIntArray())))
+        player.networkHandler.sendPacket(EntitiesDestroyS2CPacket(IntArrayList.wrap(activeIds.toIntArray())))
     }
 }
---
+++ src/main/kotlin/ua/mei/cinefabric/util/Extensions.kt
@@ -0,0 +1,5 @@
+package ua.mei.cinefabric.util
+
+import org.joml.Vector3d
+
+fun Vector3d.copy(): Vector3d = Vector3d(this.x, this.y, this.z)
